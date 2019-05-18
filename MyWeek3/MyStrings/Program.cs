3e9fee5092a29f0050714324a7dc1b3c7f75c594
elenapitul <elenapitul@gmail.com>
2019-05-18 12:28:27 +0300

    add file from visual studio

---
+++ MyWeek3/MyStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello World!";
+            for (int i = 0; i < myString.Length; i++)
+            {
+                Console.WriteLine(myString[i]);
+
+            }
+
+            for (int i = 0; i < myString.Length; i++)
+            {
+                Console.Write(myString[i]);
+            }
+
+            string aString = "My awesome string";
+            Console.WriteLine("The length of this awesome string is " + aString.Length);
+
+            string s = string.Empty;
+            string b = s;
+            Console.WriteLine(s);
+            Console.WriteLine($"b: {b}");
+
+            int myInt = 30;
+            string myIntAsString = myInt.ToString();
+            Console.WriteLine($"myIntAsString: {myIntAsString}");
+        }
+    }
+}
---
+++ MyWeek3/ReadStuff/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ReadStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string.");
+            string readedLine = Console.ReadLine();
+
+            Console.WriteLine("Please enter a second string.");
+            string readedLine2 = Console.ReadLine();
+
+            //Console.WriteLine(readedLine.Length / 2);
+            //Console.WriteLine("Hello World!");
+
+            if (readedLine == readedLine2)
+            {
+                Console.WriteLine("OK");
+            }
+
+            //Compare
+            // - 1 la stanga dpdv lexiografic
+            // 1 la dreapta dpdv lexiografic
+
+            Console.WriteLine(string.Compare(readedLine, readedLine2,true));
+
+            if (readedLine.Equals(readedLine2))
+            {
+                Console.WriteLine("Equals");
+            }
+        }
+    }
+}
